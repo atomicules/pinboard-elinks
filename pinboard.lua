2577eff365e6af0ab58d4d90a2392e2436f19f44
atomicules <atomicules@lavabit.com>
2012-12-14 09:49:25 +0000

    Escape strings using code from hooks.lua contrib
    Also:
    
    - Tidy up comments, etc, put Todos at the bottom
    - Use XDialog for tags and description
    - Make read later work by using &next=same url parameter

--- pinboard.lua
+++ pinboard.lua
@@ -1,43 +1,69 @@
---Save current page to pinboard
+--Pinboard tools for ELinks
 --source from your ~/.elinks/hooks.lua file with `dofile("pinboard.lua")`
 
-function addto_pinboard (description)
+--ALT+p to save a bookmark. Brings up an XDialog, first row is to enter a description, second row is for tags
+--ALT+l to read later. 
+
+
+function addto_pinboard (description, tags)
 	pinboardBase = 'https://pinboard.in/add?'
 	url = current_url ()
 	title = current_title ()
-	docURL = pinboardBase..'url='..encodeURIComponent(url)..'&title='..encodeURIComponent(title)..'&description='..encodeURIComponent(description)..' ','Pinboard','toolbar=no,width=700,height=350';
+	docURL = pinboardBase..'url='..escape (url)..'&title='..escape (title)..'&description='..escape (description)..'&tags='..escape (tags)..' ','Pinboard','toolbar=no,width=700,height=350';
     return docURL
 end
 	bind_key ("main", "Alt-p",
-		function () return "goto_url", addto_pinboard () end)
-	--Alt key is the Apple key on OSX
-
-	bind_key ("main", "Alt-p",
-		function () xdialog("",
-			function (description)
+		function () xdialog("<Description>", "<Tags>",
+			function (description, tags)
 				return "goto_url",
-				addto_pinboard (description) 
+				addto_pinboard (description, tags) 
 		end)
 	end)
+
 
 function readlater_pinboard ()
 	pinboardBase = 'https://pinboard.in/add?'
 	url = current_url ()
 	title = current_title ()
-	docURL = pinboardBase..'later=yes&noui=yes&jump=close&url='..encodeURIComponent(url)..'&title='..encodeURIComponent(title)..' ','Pinboard','toolbar=no,width=100,height=100';
+	docURL = pinboardBase..'later=yes&next=same&noui=yes&jump=close&url='..escape (url)..'&title='..escape (title)..' ','Pinboard','toolbar=no,width=100,height=100';
+	--Uses &next=same to immediately return to page being bookmarked
     return docURL
 end
 	bind_key ("main", "Alt-l",
 		function () return "goto_url", readlater_pinboard () end)
 
 
-function encodeURIComponent (component)
-	component = string.gsub(component, "%s", "%%21")
-	component = string.gsub(component, "'", "%%27")
-	component = string.gsub(component, "%(", "%%28")
-	component = string.gsub(component, "%)", "%%29")
-	component = string.gsub(component, "%*", "%%2A")
-	return component
+--The following taken from the contrib hooks.lua sample
+function hx (c)
+    return string.char((c >= 10 and (c - 10) + string.byte ('A')) or c + string.byte ('0'))
 end
 
+function char2hex (c)
+    return '%'..hx (string.byte (c) / 16)..hx (math.mod(string.byte (c), 16))
+end
 
+function escape (str)
+    return string.gsub (str, "(%W)", char2hex)
+end
+--end stealing
+
+
+--Todo and Ideas
+--==============
+
+--Always redirect to mobile site? But this conflicts with the actual bookmarking up above. So maybe not!
+--[[
+function follow_url_hook(url)
+	if string.find(url, "://pinboard.in") then
+		return "http://m.pinboard.in"
+	else
+		return url
+	end
+end
+--]]
+
+--Re-format the mobile site, remove any buttons that use javascript since that doesn't work in ELinks. Also remove stuff where CSS is "display: none", but ELinks is displaying anyway. Just make nice and basic.
+
+--Re-format the mobile site and see if can add Lua implementatons for deleting, editing bookmarks. For deleting just need to make a POST. If can't do this in browser could do it in Curl, via Lua.
+
+--Tab sets would be cool, but don't think there is anyway to get list of open tabs via Lua interface? Perhaps read session snapshot from bookmarks file? Hmmm...
