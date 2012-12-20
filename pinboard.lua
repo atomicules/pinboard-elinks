bff460d04990e91d1164d5c9a0f25187b6c51ca8
atomicules <atomicules@lavabit.com>
2012-12-20 00:04:29 +0000

    Add delete "button" to bookmark page, works via Pinboard API
    - Can't use normal delete/destroy since requires posting form data and
      as far as I know that can only be done via Javascript. However, the
      API works great just as a get request. Therefore make a delete link
      that works via an API call
    - Since this is immediately destructive only put the delete link on the
      individual bookmark page, not when browsing through a list of
      bookmarks
    - Requires API token to be entered into file
    - Also use some file level variables to avoid repetition and
      duplication.
    - Clear up naming. Was a mix of camel case and underscores

--- pinboard.lua
+++ pinboard.lua
@@ -5,13 +5,16 @@
 --ALT+l to read later. 
 --Reformats the mobile site to be better for ELinks
 
+--In order to be able to delete need to enter auth_token below
+local pb_auth = ""
+local pb_base = 'https://pinboard.in/add?'
+local pb_url
 
 function addto_pinboard (description, tags)
-	pinboardBase = 'https://pinboard.in/add?'
 	url = current_url ()
 	title = current_title ()
-	docURL = pinboardBase..'url='..escape (url)..'&title='..escape (title)..'&description='..escape (description)..'&tags='..escape (tags)..'&next='..escape (url)..' ','Pinboard','toolbar=no,width=700,height=350';
-    return docURL
+	doc_url = pb_base..'url='..escape (url)..'&title='..escape (title)..'&description='..escape (description)..'&tags='..escape (tags)..'&next='..escape (url)..' ','Pinboard','toolbar=no,width=700,height=350';
+    return doc_url
 end
 	bind_key ("main", "Alt-p",
 		function () xdialog("<Description>", "<Tags>",
@@ -23,12 +26,11 @@ end
 
 
 function readlater_pinboard ()
-	pinboardBase = 'https://pinboard.in/add?'
 	url = current_url ()
 	title = current_title ()
-	docURL = pinboardBase..'later=yes&next=same&noui=yes&jump=close&url='..escape (url)..'&title='..escape (title)..' ','Pinboard','toolbar=no,width=100,height=100';
+	doc_url = pb_base..'later=yes&next=same&noui=yes&jump=close&url='..escape (url)..'&title='..escape (title)..' ','Pinboard','toolbar=no,width=100,height=100';
 	--Uses &next=same to immediately return to page being bookmarked
-    return docURL
+    return doc_url
 end
 	bind_key ("main", "Alt-l",
 		function () return "goto_url", readlater_pinboard () end)
@@ -37,10 +39,11 @@ end
 function pre_format_html_hook (url, html)
 	--strip stuff that shouldnae be displayed anyway
 	if string.find(url, "://m.pinboard.in") then
+		pb_url = url --since can't pass additional args to new_edit_link
 		html = string.gsub (html, '<div name="edit_checkbox".-</div>', '') --ok as no child divs
 		html = string.gsub (html, '<div class="star.-</div>', '') --ok as no child divs
 		html = string.gsub (html, '<fieldset id="bulk_edit_box">.-</fieldset>', '') --do this instead of parent div
-		html = string.gsub (html, '<a class="bookmark_title.-edit</a>', new_edit_link )
+		html = string.gsub (html, '<a class="bookmark_title.-edit</a>', new_edit_link)
 		html = string.gsub (html, '<div class="delete_link".-</div>', '')
 		html = string.gsub (html, '<div style="display:inline" class="read">.-</div>', '')
 		html = string.gsub (html, '<div id="edit_bookmark_form".-</form>\n  \n</div>', '')
@@ -58,8 +61,12 @@ function new_edit_link (s)
 	_,_,link = string.find (s, '<a class="bookmark_title.-href="(.-)"')
 	--But need to escape any %X (where X is a digit) in the URL http://stackoverflow.com/a/6705995/208793
 	link = string.gsub (link, "([%%])", "%%%1")
-	pbbase = "https://pinboard.in/add?url="
-	replacement = string.gsub (s, '<a onclick="edit.-</a>', '<a href="'..pbbase..link..'" class="edit">Edit</a><br>')
+	if string.find(pb_url, "/b:") and not (pb_auth == "") then --if on individual bm page, add Delete button as well
+		delete_link = '<a href="https://api.pinboard.in/v1/posts/delete?url='..link..'&auth_token='..pb_auth..'" class="delete">Delete</a>'
+	else
+		delete_link = ''
+	end
+	replacement = string.gsub (s, '<a onclick="edit.-</a>', '<a href="'..pb_base..'url='..link..'" class="edit">Edit</a> '..delete_link..'<br>')
 	return replacement
 end
 
