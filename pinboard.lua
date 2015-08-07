f5d9111f6502fe285e097947a652774d3e9d1ed0
atomicules <base@atomicules.co.uk>
2015-08-07 23:04:51 +0100

    Revert commits 7036095 and 4fd8445
    Figured out you can append to Lua functions so I'll leave this for
    Pinboard only and append to it in my hooks.lua file.

--- pinboard.lua
+++ pinboard.lua
@@ -54,15 +54,6 @@ function pre_format_html_hook (url, html)
 		html = string.gsub (html, '<div style="display:inline" id="delete_.-</div>', '')
 		html = string.gsub (html, '<div style="visibility:hidden;display:none" class="delete_div" id="destroy_.-</div>', '')
 		return html
-	--Fix none, wrapping pages. For now, just this one
-	--This is nothing to do with Pinboard, but I can only have one pre_format_html_hook
-	elseif string.find(url, "antirez") then
-		--Change <pre> to <div> this is very hacky and prone to failure
-		html = string.gsub (html, '<pre(.-)</pre>', '<div%1</div>')
-		--html = string.gsub (html, '<pre(.-)</pre>.-</div><div.->', '<div%1</div>')
-		--But since we only want to do this for pre not followed by code, change any divs followed by code back to pre
-		html = string.gsub (html, '<div(.-<code.-</code>.-)</div>', '<pre%1</pre>')
-		return html
 	end
 end
 
